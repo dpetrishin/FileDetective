305a9b63ce978912515646826f000a8a34d687d3
Dmitry Petrishin <>
2018-09-21 15:17:24 +0300

    Initial.

---
+++ FileDetective.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileDetective.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
